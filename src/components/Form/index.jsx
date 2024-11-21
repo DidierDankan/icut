8ca7c3b599a75b682200badaa6e9cd73bc83c955
didier <didier.dankan@alphanetworks.global>
2024-11-21 11:00:26 +0100

    new form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,6 +3,8 @@ import emailjs from 'emailjs-com';
 
 const Form = ({isHome=false}) => {
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [lName, setLName] = useState('');
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(null);
 
@@ -19,8 +21,10 @@ const Form = ({isHome=false}) => {
             e.target    // Pass the form as the target
         );
 
-        console.log(result.text);
-        setSuccess('Thank you, we will remember this!');
+        setSuccess('Thank you for your trust!');
+        setEmail('')
+        setName('')
+        setLName('')
         } catch (error) {
         console.error('Error sending email:', error);
         setSuccess('Error sending email. Please try again later.');
@@ -30,12 +34,33 @@ const Form = ({isHome=false}) => {
     };
     return (
         <form className={isHome ? 'hero-form' : null} onSubmit={handleSubmit}>
-            <label htmlFor="email">{isHome ? "Get ahead and pre-signup:" :"Email Address:"}</label>
+            <div className='flex'>
+                <input
+                style={{marginRight: "10px", width: "150px"}}
+                type="text"
+                id="name"
+                name="user_name"
+                placeholder="First Name"
+                value={name}
+                onInput={(e) => setName(e.target.value)}
+                required
+                />
+                <input
+                style={{marginRight: "10px", width: "150px"}}
+                type="text"
+                id="lname"
+                name="user_lName"
+                placeholder="Last Name"
+                value={lName}
+                onInput={(e) => setLName(e.target.value)}
+                required
+                />
+            </div>
             <input
               type="email"
               id="email"
               name="user_email"
-              placeholder="Enter your email..."
+              placeholder="Email Address"
               value={email}
               onInput={(e) => setEmail(e.target.value)}
               required
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,49 @@
-import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import { Link } from 'preact-router/match';  // If you are using preact-router for navigation
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      <li>
-        <Link href="/" className="nav-link">Home</Link>
-      </li>
-      <li>
-        <Link href="/about" className="nav-link">About</Link>
-      </li>
-      <li>
-        <Link href="/pre-signup" className="nav-link">Signup</Link>
-      </li>
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const linkClick = (href) => {
+    setMenuOpen(false)
+  }
+
+  return (<nav className="navbar">
+    <div className='logo'>
+      <Link href="/">OnYou</Link>
+    </div>
+
+    {/* Hamburger Icon */}
+    <div className="hamburger" id="hamburger-icon" onClick={toggleMenu}>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+
+    <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+      <Link href="/" className="nav-link" onClick={linkClick}>
+        <li>
+          Home
+        </li>
+      </Link>
+      <Link href="/about" className="nav-link" onClick={linkClick}>
+        <li>
+          About
+        </li>
+      </Link>
+      <Link href="/pre-signup" className="nav-link" onClick={linkClick}>
+        <li>
+          Signup
+        </li>
+      </Link>
     </ul>
   </nav>
-);
+  )
+};
 
 export default Navbar;
